5e490c98411fd8c40522f73043c1e4cd3799cf25
Stanislav Kazakov <kazakovsv@tut.by>
2020-07-04 14:05:00 +0300

    Add test model PersonInfo

---
+++ CustomPropertyGridEditors/P001_CustomEnumComboBoxEditor/PersonInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace P001_CustomEnumComboBoxEditor
+{
+    public enum Gender
+    {
+        [Description("Женщина")]
+        Female,
+
+        [Description("Мужчина")]
+        Male
+    }
+
+    public class PersonInfo
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public Gender Sex { get; set; }
+
+        public PersonInfo()
+        {
+            FirstName = "Unknown";
+            LastName = "Unknown";
+            Age = 1;
+            Sex = Gender.Female;
+        }
+    }
+}
